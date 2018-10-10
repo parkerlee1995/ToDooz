076f21490297ba2b778b6ea908a585d1b94d9a16
Parker Lee <parker.lee.1995@gmail.comm>
2018-10-09 21:55:46 -0500

    Data persisted using UserDefaults method.

--- ToDooz/ToDoListViewController.swift
+++ ToDooz/ToDoListViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy eggos", "Destroy demegorgons"]
+    var itemArray = ["Find Mike", "Buy eggos", "Destroy demegorgons"]
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //User defaults to persist data
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK - Tableview datasource methods
@@ -46,6 +52,35 @@ class ToDoListViewController: UITableViewController {
         //Glay flash when clicking on a row
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    //MARK - Add new items
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new item to list", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (alert) in
+            
+            //What will happen once the user clicks the Add Item button on our UIAlert
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
